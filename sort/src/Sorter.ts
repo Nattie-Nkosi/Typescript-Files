4dc10760fd4f6eea2cee30c94a8bffb6244b12c8
Nutty Nkosi <nkosin361@gmail.com>
2021-01-12 20:11:57 +0200

    Learning LinkedLists 😥

--- sort/src/Sorter.ts
+++ sort/src/Sorter.ts
@@ -9,6 +9,7 @@ export class Sorter {
 
   sort():void {
     const { length } = this.collection;
+    
     for(let i = 0; i < length; i++){
       for(let j = 0; j < length - i - 1; j++){
           if(this.collection.compare(j, i + 1)){
