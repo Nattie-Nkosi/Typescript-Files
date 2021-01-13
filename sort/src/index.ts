997701a0cb789ccdf34003f2a3c702cfd43b3f2f
Nutty Nkosi <nkosin361@gmail.com>
2021-01-13 21:34:49 +0200

    Understanding Abstract Classes and Inheritance 🚀

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,15 +1,24 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-const numbersCollection = new NumbersCollection([-12, 5, 9, 0, 300, 14]);
-const charactersCollection = new CharactersCollection('Xaayb');
-const sorter = new Sorter(numbersCollection);
-const sorterString = new Sorter(charactersCollection);
-sorter.sort();
+
+const numbersCollection = new NumbersCollection([1,4,-2,55,100,-33]);
+numbersCollection.sort();
 console.log(numbersCollection.data);
-sorterString.sort();
-console.log(charactersCollection.data);
+
+const charsCollection = new CharactersCollection('Xaaby');
+charsCollection.sort();
+console.log(charsCollection.data);
+
+const linkedlist = new LinkedList();
+linkedlist.add(-32);
+linkedlist.add(0);
+linkedlist.add(99);
+linkedlist.add(54);
+linkedlist.sort();
+linkedlist.print();
 
 
 
